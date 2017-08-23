04eed873bedfa8e62ed6f0baac39b3fcff6b237a
shalomcrown <shalomc@infinidat.com>
2017-08-23 21:44:16 +0300

    Added getting datasets (untested)

--- com.kirayim.metstat/metstat.py
+++ com.kirayim.metstat/metstat.py
@@ -66,12 +66,20 @@ class MetApp:
         self.cityCombo = ttk.Combobox(master=frame, textvariable=self.cityVar)
         self.cityCombo.grid(row=2, column=2)
         Button(frame, text="Get cities", command=self.get_cities).grid(row=2, column=3)
-
-        Button(frame, text="QUIT", fg="red", command=frame.quit).grid(row=3, column=1)
-        Button(frame, text="Enter token", command=self.enter_token).grid(row=3, column=2)
+        
+        
+        Label(frame, text="Datasets").grid(row=3, column=1)
+        self.categoryVar = StringVar(master)
+        self.categories = {}
+        self.categoryCombo = ttk.Combobox(master=frame, textvariable=self.categoryVar)
+        self.categoryCombo.grid(row=3, column=2)
+        Button(frame, text="Get datasets", command=self.get_city_categories).grid(row=3, column=4)
+        
+        Button(frame, text="QUIT", fg="red", command=frame.quit).grid(row=8, column=1)
+        Button(frame, text="Enter token", command=self.enter_token).grid(row=8, column=2)
 
         self.progressBar = ttk.Progressbar(frame, mode='determinate')
-        self.progressBar.grid(row=4, column=0, columnspan=6, sticky=W+E)
+        self.progressBar.grid(row=9, column=0, columnspan=6, sticky=W+E)
 
         self.load_token()
 
@@ -146,6 +154,29 @@ class MetApp:
     def get_cities(self):
         threading.Thread(target=self.get_cities_thread).start()
 
+
+    #--------------------------------------------------------------------------------------------
+
+    def getDataCategoriesThread(self, locationId):
+        for categoryData in self.get_all_data("datasets?datatypeid=TOBS&locationId=" + locationId, blocksize):
+            categoryName = "{} {} {}".format(categoryData['name'], categoryData['mindate'], categoryData['maxDate'])
+            self.categories[categoryName] = categoryData
+            self.categoryCombo['values'] = sorted(self.categories.keys())
+
+
+    def getDataCategories(self, locationId):
+        threading.Thread(target=self.getTempDataThread, args=(locationId))
+
+    #--------------------------------------------------------------------------------------------
+
+    def get_city_categories(self):
+        cityName = self.cityVar.get()
+        cityData = self.cities[cityName]
+        self.getDataCategories(cityData['id'])
+
+
+
+    #--------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG, format ='[%(levelname)s] %(asctime)s  %(filename)s(%(lineno)d)  %(funcName)s %(message)s')    
